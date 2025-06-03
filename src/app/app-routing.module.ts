a60e16fbe5300ab1535d918baa6364b87fff7043
Eduardo <eduardoEmpresa09@gmail.com>
2025-06-03 18:05:50 +0200

    Changes by jgsanchez

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,126 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
+    path: 'dashboard',
+    title: 'Dashboard',
+    component: ProjectLayoutComponent,
+    loadChildren: () => import('./features/dashboard/main-dashboard/main-dashboard.module').then(m => m.MainDashboardModule),
+  },
+  {
+    path: 'users',
+    title: 'Users',
+    component: ProjectLayoutComponent,
+    canMatch: [IsAdminGuard],
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./features/users/components/users-list/users.module').then(m => m.UsersModule),
+      },
+      {
+        path: 'createUser',
+        title: 'Create User',
+        loadChildren: () => import('./features/users/components/create-user/create-user.module').then(m => m.CreateUserModule),
+      },
+      {
+        path: 'updateUser/:idUser',
+        title: 'Update User',
+        loadChildren: () => import('./features/users/components/update-user/update-user.module').then(m => m.UpdateUserModule),
+      },
+      {
+        path: ':idUser',
+        loadChildren: () => import('./features/users/components/user-details/user-details.module').then(m => m.UserDetailsModule),
+      },
+    ],
+  },
+  {
+    path: 'customers',
+    title: 'Customers',
+    component: ProjectLayoutComponent,
+    canMatch: [IsAdminGuard],
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./features/customers/components/customer-list/customer.module').then(m => m.CustomerModule),
+      },
+      {
+        path: 'createCustomer',
+        title: 'Create Customer',
+        loadChildren: () => import('./features/customers/components/create-customer/create-customer.module').then(m => m.CreateCustomerModule),
+      },
+      {
+        path: 'updateCustomer/:idCustomer',
+        title: 'Update Customer',
+        loadChildren: () => import('./features/customers/components/update-customer/update-customer.module').then(m => m.UpdateCustomerModule),
+      },
+      {
+        path: ':idCustomer',
+        loadChildren: () => import('./features/customers/components/customer-details/customer-details.module').then(m => m.CustomerDetailsModule),
+      },
+
+    ]
+  },
+  {
+    path: 'projects',
+    title: 'Projects',
+    component: ProjectLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./features/projects/components/projects-list/projects.module').then(m => m.ProjectsModule),
+      },
+      {
+        path: 'createProject',
+        canMatch: [IsAdminGuard],
+        title: 'Create Project',
+        loadChildren: () => import('./features/projects/components/create-project/create-project.module').then(m => m.CreateProjectModule),
+      },
+      {
+        path: 'updateProject/:idProject',
+        title: 'Update Project',
+        canMatch: [IsAdminGuard],
+        loadChildren: () => import('./features/projects/components/update-project/update-project.module').then(m => m.UpdateProjectModule),
+      },
+      {
+        path: ':idProject',
+        loadChildren: () => import('./features/projects/components/project-details/project-details.module').then(m => m.ProjectDetailsModule),
+      },
+
+    ]
+  },
+  {
+    path: 'tasks',
+    title: 'Tasks',
+    component: ProjectLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./features/tasks/components/tasks-list/tasks.module').then(m => m.TasksModule),
+      },
+      {
+        path: 'createTask',
+        canMatch: [IsAdminGuard],
+        title: 'Create Task',
+        loadChildren: () => import('./features/tasks/components/create-task/create-task.module').then(m => m.CreateTaskModule),
+      },
+      {
+        path: 'updateTask/:idTask',
+        title: 'Update Task',
+        loadChildren: () => import('./features/tasks/components/update-task/update-task.module').then(m => m.UpdateTaskModule),
+      },
+    ]
+  },
+  {
+    path: 'tasks/:idTask',
+    component: FullscreenLayoutComponent,
+    loadChildren: () => import('./features/tasks/components/task-details/task-details.module').then(m => m.TaskDetailsModule),
+  },
+  {
+    path: 'notifications',
+    title: 'Notifications',
+    component: ProjectLayoutComponent,
+    loadChildren: () => import('./features/notifications/components/notifications-list/notifications-list.module').then(m => m.NotificationsListModule),
+  },
+  {
     path: '',
     component: FullscreenLayoutComponent,
     children: [
@@ -26,144 +146,6 @@ const routes: Routes = [
       },
     ]
   },
-  {
-    path: '',
-    component: ProjectLayoutComponent,
-    children: [
-      {
-        path: 'dashboard',
-        title: 'Dashboard',
-        loadChildren: () => import('./features/dashboard/main-dashboard/main-dashboard.module').then(m => m.MainDashboardModule),
-      },
-      {
-        path: 'users',
-        title: 'Users',
-        canMatch: [IsAdminGuard],
-        children: [
-          {
-            path: '',
-            loadChildren: () => import('./features/users/components/users-list/users.module').then(m => m.UsersModule),
-          },
-          {
-            path: 'createUser',
-            title: 'Create User',
-            loadChildren: () => import('./features/users/components/create-user/create-user.module').then(m => m.CreateUserModule),
-          },
-          {
-            path: 'updateUser/:idUser',
-            title: 'Update User',
-            loadChildren: () => import('./features/users/components/update-user/update-user.module').then(m => m.UpdateUserModule),
-          },
-          {
-            path: ':idUser',
-            loadChildren: () => import('./features/users/components/user-details/user-details.module').then(m => m.UserDetailsModule),
-          },
-
-        ],
-
-      },
-      {
-        path: 'customers',
-        title: 'Customers',
-        canMatch: [IsAdminGuard],
-        children: [
-          {
-            path: '',
-            loadChildren: () => import('./features/customers/components/customer-list/customer.module').then(m => m.CustomerModule),
-          },
-          {
-            path: 'createCustomer',
-            title: 'Create Customer',
-            loadChildren: () => import('./features/customers/components/create-customer/create-customer.module').then(m => m.CreateCustomerModule),
-          },
-          {
-            path: 'updateCustomer/:idCustomer',
-            title: 'Update Customer',
-            loadChildren: () => import('./features/customers/components/update-customer/update-customer.module').then(m => m.UpdateCustomerModule),
-          },
-          {
-            path: ':idCustomer',
-            loadChildren: () => import('./features/customers/components/customer-details/customer-details.module').then(m => m.CustomerDetailsModule),
-          },
-
-        ]
-      },
-      {
-        path: 'projects',
-        title: 'Projects',
-        children: [
-          {
-            path: '',
-            loadChildren: () => import('./features/projects/components/projects-list/projects.module').then(m => m.ProjectsModule),
-          },
-          {
-            path: 'createProject',
-            canMatch: [IsAdminGuard],
-            title: 'Create Project',
-            loadChildren: () => import('./features/projects/components/create-project/create-project.module').then(m => m.CreateProjectModule),
-          },
-          {
-            path: 'updateProject/:idProject',
-            title: 'Update Project',
-            canMatch: [IsAdminGuard],
-            loadChildren: () => import('./features/projects/components/update-project/update-project.module').then(m => m.UpdateProjectModule),
-          },
-          {
-            path: ':idProject',
-            loadChildren: () => import('./features/projects/components/project-details/project-details.module').then(m => m.ProjectDetailsModule),
-          },
-
-        ]
-      },
-      {
-        path: 'tasks',
-        title: 'Tasks',
-        children: [
-          {
-            path: '',
-            loadChildren: () => import('./features/tasks/components/tasks-list/tasks.module').then(m => m.TasksModule),
-          },
-          {
-            path: 'createTask',
-            canMatch: [IsAdminGuard],
-            title: 'Create Task',
-            loadChildren: () => import('./features/tasks/components/create-task/create-task.module').then(m => m.CreateTaskModule),
-          },
-          {
-            path: 'updateTask/:idTask',
-            title: 'Update Task',
-            loadChildren: () => import('./features/tasks/components/update-task/update-task.module').then(m => m.UpdateTaskModule),
-          },
-          {
-            path: ':idTask',
-            loadChildren: () => import('./features/tasks/components/task-details/task-details.module').then(m => m.TaskDetailsModule),
-          },
-
-        ]
-      },
-      {
-        path: 'attachments',
-        title: 'Attachments',
-        children: [
-          {
-            path: '',
-            loadChildren: () => import('./features/tasks/attachments/components/attachments-list/attachments-list.module').then(m => m.AttachmentsListModule),
-          },
-        ]
-      },
-      {
-        path: 'notifications',
-        title: 'Notifications',
-        loadChildren: () => import('./features/notifications/components/notifications-list/notifications-list.module').then(m => m.NotificationsListModule),
-      }
-    ]
-  },
-  {
-    path: 'register',
-    title: 'Register',
-    loadChildren: () => import('./features/auth/pages/register-page/register-page.module').then(m => m.RegisterPageModule),
-  },
-
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
-import {HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient, withInterceptors} from '@angular/common/http';
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouteReuseStrategy} from '@angular/router';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
 import {AuthService} from "./core/auth/services/auth-service.service";
 import {authInterceptor} from "./core/auth/interceptors/auth-interceptor.interceptor";
 import {ProjectLayoutComponent} from "./project-layout/project-layout.component";
 import {NotificationIconModule} from "./features/notifications/components/notification-icon/notification-icon.module";
+import {FullscreenLayoutComponent} from "./fullscreen-layout/fullscreen-layout.component";
+import {CommonModule} from "@angular/common";
 
 @NgModule({
-  declarations: [AppComponent, ProjectLayoutComponent],
-    imports: [
-        BrowserModule,
-        IonicModule.forRoot(),
-        AppRoutingModule,
-        HttpClientModule,
-        NotificationIconModule,
-    ],
+  declarations: [AppComponent, ProjectLayoutComponent, FullscreenLayoutComponent],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    NotificationIconModule,
+  ],
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
     provideHttpClient(withInterceptors([
-        authInterceptor
-      ])),
+      authInterceptor
+    ])),
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
